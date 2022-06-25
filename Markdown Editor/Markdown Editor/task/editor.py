4fd5f826074026c3032973d4b57145bd6e4db812
Thái Bảo Tài <thaibaotai@gmail.com>
2022-06-26 00:32:27 +0700

    save

--- Markdown Editor/Markdown Editor/task/editor.py
+++ Markdown Editor/Markdown Editor/task/editor.py
@@ -3,6 +3,8 @@ import sys
 available_formatters = ['plain', 'bold', 'italic', 'header', 'link', 'inline-code',
                         'new-line']
 special_commands = ['!help', '!done']
+output = ''
+formatter = ''
 
 
 def help_print():
@@ -15,54 +17,73 @@ def done():
 
 
 def check_input():
-    if formatter not in available_formatters + special_commands:
-        print('Unknown formatting type or command')
-    if formatter == '!help':
-        help_print()
-    if formatter == '!done':
-        done()
+    while True:
+        global formatter
+        formatter = input('Choose a formatter: ')
+        if formatter not in available_formatters + special_commands:
+            print('Unknown formatting type or command')
+        if formatter == '!help':
+            help_print()
+        if formatter == '!done':
+            done()
+        else:
+            break
 
 
 def format_text():
-    header() if formatter == 'header'
-
-
+    if formatter == 'header': return header()
+    if formatter == 'plain': return plain()
+    if formatter == 'bold': return bold()
+    if formatter == 'italic': return italic()
+    if formatter == 'new-line': return new_line()
+    if formatter == 'link': return link()
+    if formatter == 'inline-code': return inline_code()
 
 
 def header():
     level = int(input('Level: '))
-    text = input()
-    print('#' * level + text)
+    while level not in range(1, 7):
+        print('The level should be within the range of 1 to 6')
+        level = int(input('Level: '))
+    else:
+        text = input('Text: ')
+    if output == '':
+        return '#' * level + ' ' + text + '\n'
+    else:
+        return '\n' + '#' * level + ' ' + text + '\n'
 
 
 def plain():
-    text = input()
-    print(text)
+    text = input('Text: ')
+    return text
+
 
 def bold():
-    text = input()
-    print('**' + text + '**')
+    text = input('Text: ')
+    return '**' + text + '**'
 
 
 def italic():
-    text = input()
-    print('*' + text + '*')
+    text = input('Text: ')
+    return '*' + text + '*'
 
 
 def new_line():
-    print()
+    return '\n'
 
 
 def link():
     label = input('Label: ')
     url = input('URL: ')
-    print(f'[{label}]({url})')
+    return f'[{label}]({url})'
 
 
 def inline_code():
-    text = input()
-    print('>' + text)
+    text = input('Text: ')
+    return '`' + text + '`'
+
 
 while True:
-    formatter = input('Choose a formatter: ')
     check_input()
+    output += format_text()
+    print(output)
